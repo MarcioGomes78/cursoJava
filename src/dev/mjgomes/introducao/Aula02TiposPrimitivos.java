64292b55042719dce6d24904c5a8b88d31f7453a
MarcioGomes78 <mjgomes1978@gmail.com>
2022-08-03 17:07:09 -0300

    Tipos primitivo testado.

--- src/dev/mjgomes/introducao/Aula02TiposPrimitivos.java
+++ src/dev/mjgomes/introducao/Aula02TiposPrimitivos.java
@@ -2,6 +2,9 @@ package dev.mjgomes.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args){
-        
+        // byte, boolean, char, double, float, int, long, short
+        int idade = 44;
+        String nome = "Marcio";
+        System.out.println("A idade do " + nome + " é " + idade + " anos.");
     }
 }
