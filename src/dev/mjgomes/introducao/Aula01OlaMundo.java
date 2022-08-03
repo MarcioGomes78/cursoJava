53fec6ecdf9a876d2f9cc53c73cdbd292d8c11bc
MarcioGomes78 <mjgomes1978@gmail.com>
2022-08-03 13:17:32 -0300

    Iniciando curso de java.

---
+++ src/dev/mjgomes/introducao/Aula01OlaMundo.java
@@ -0,0 +1,7 @@
+package dev.mjgomes.introducao;
+
+public class Aula01OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Olá, sou um novo dev!");
+    }
+}
---
+++ src/dev/mjgomes/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,7 @@
+package dev.mjgomes.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args){
+        
+    }
+}
