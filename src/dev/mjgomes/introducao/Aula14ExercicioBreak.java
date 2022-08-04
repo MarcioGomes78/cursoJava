a0fa3f0c7ed8a73e1b12704fa001649de12a7774
MarcioGomes78 <mjgomes1978@gmail.com>
2022-08-04 17:57:50 -0300

    Exercicio break

---
+++ src/dev/mjgomes/introducao/Aula14ExercicioBreak.java
@@ -0,0 +1,17 @@
+package dev.mjgomes.introducao;
+// Dado o valor de um carro, descubra em quantas vezes ele pode ser parcelado
+// Condição: valorParcela >= 1000
+public class Aula14ExercicioBreak {
+    public static void main(String[] args) {
+
+        double valorCarro = 50000;
+
+        for (int parcela = 1; parcela <= valorCarro; parcela++) {
+            double valorParcela = valorCarro / parcela;
+            if (valorParcela < 1000) {
+                break;
+            }
+            System.out.println("Parcela: " + parcela + " R$ " + valorParcela);
+        }
+    }
+}
